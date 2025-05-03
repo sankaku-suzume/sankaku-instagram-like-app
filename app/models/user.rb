d75da4853e0e564287d059de66fa71849d2b18af
さんかく <sankaku.suzume@gmail.com>
2025-05-03 18:07:51 +0900

    accountはuniqueに

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :account, uniqueness: true
 end
