cb78ecbcf9f769e6851415191f72e51e852f9c9f
さんかく <sankaku.suzume@gmail.com>
2025-05-21 22:01:18 +0900

    Babelプラグイン設定

--- babel.config.js
+++ babel.config.js
@@ -42,7 +42,19 @@ module.exports = function(api) {
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       [
-        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-transform-class-properties',
+        {
+          loose: true
+        }
+      ],
+      [
+        '@babel/plugin-transform-private-methods',
+        {
+          loose: true
+        }
+      ],
+      [
+        '@babel/plugin-transform-private-property-in-object',
         {
           loose: true
         }
