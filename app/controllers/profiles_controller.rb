9518040068cb72ccd1d7053bbcbbf7ff332c2854
さんかく <sankaku.suzume@gmail.com>
2025-05-26 05:45:55 +0900

    profileのedit,update作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,26 @@ class ProfilesController < ApplicationController
   def show
     @profile = current_user.profile
   end
+
+  def edit
+    @profile = current_user.prepare_profile
+  end
+
+  def update
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'Successfully updated.'
+    else
+      flash.now[:error] = 'Failed to update.'
+      render :edit
+    end
+   end
+
+   private
+   def profile_params
+     params.require(:profile).permit(
+      :avatar
+     )
+   end
 end
