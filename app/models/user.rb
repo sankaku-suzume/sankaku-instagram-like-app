cf8de253b891aa40fb445b7945940ad98566f308
さんかく <sankaku.suzume@gmail.com>
2025-06-15 17:46:31 +0900

    likeモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_one :profile, dependent: :destroy
 
   validates :account, uniqueness: true
