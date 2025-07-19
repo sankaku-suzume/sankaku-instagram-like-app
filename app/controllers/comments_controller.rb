820c41164d22fb1d9c96a22babcde87999d9c2d6
さんかく <sankaku.suzume@gmail.com>
2025-07-19 14:10:40 +0900

    appsのrouting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
   def index
     @article = Article.find(params[:article_id])
     @comments = @article.comments
