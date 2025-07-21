daa1649c180f86d9c8ac41e4a915e7af1f510e52
さんかく <sankaku.suzume@gmail.com>
2025-07-21 15:56:56 +0900

    userモデルのテスト、バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,8 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
 
   validates :account, uniqueness: true
+  validates :account, presence: true
+  validates :account, format: { with: /\S/ }
 
   def has_written?(article)
     articles.exists?(id: article.id)
