a2eae0558de32cb21eb0c7397769968153e14816
さんかく <sankaku.suzume@gmail.com>
2025-07-25 12:04:52 +0900

    articleのテストモデル作成

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,27 +4,10 @@ RSpec.describe Article, type: :model do
   let!(:user) { create(:user) }
 
   context '画像が1つ以上選択されている場合' do
-    let!(:article) do
-      user.articles.build({
-        images: fixture_file_upload('default-avatar.png')
-      })
-    end
+    let!(:article) { build(:article, user: user) }
 
     it '記事を保存できる' do
       expect(article).to be_valid
     end
   end
-
-  # context "画像が選択されていない場合" do
-  #   let!(:article) do
-  #     user.articles.create({
-  #       images: nil
-  #     })
-  #   end
-
-  #   # it '記事を保存できない' do
-  #   #   expect(article.errors.messages)
-  #   # end
-  # end
-  
 end
