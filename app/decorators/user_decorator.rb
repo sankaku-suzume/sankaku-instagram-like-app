25ab3420f6c57a50d698d33c5df6c5c54895b498
さんかく <sankaku.suzume@gmail.com>
2025-07-30 05:27:06 +0900

    comment #index、#createのテスト

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 module UserDecorator
-  def avatar_image
-    if profile&.avatar&.attached?
-      profile.avatar
-    else
-      'default-avatar.png'
-    end
-  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -66,6 +66,14 @@ class User < ApplicationRecord
     profile || build_profile
   end
 
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
+  end
+
   private
   def get_user_id(user)
     if user.is_a?(User)
