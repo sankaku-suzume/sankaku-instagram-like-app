c4c5b074841af3ab22df7d28c3ea7da51cc4863f
さんかく <sankaku.suzume@gmail.com>
2025-08-17 18:04:26 +0900

    active storageのマイグレーションファイルが消失していることへの対応

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379') }
-# end
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379') }
+end
 
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379') }
-# end
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379') }
+end
 
 Sidekiq.configure_server do |config|
   config.redis = {
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint           not null, primary key
+#  content    :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
+#
 FactoryBot.define do
   factory :article do
     content { Faker::Lorem.characters(number: 10) }
