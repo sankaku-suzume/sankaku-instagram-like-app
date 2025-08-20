0f914c868f2937b8078df5ca686d3a02dd08e6c3
さんかく <sankaku.suzume@gmail.com>
2025-08-21 05:54:17 +0900

    いいねの数を有効数字2桁に丸める

--- app/models/article.rb
+++ app/models/article.rb
@@ -26,10 +26,22 @@ class Article < ApplicationRecord
   def random_liked_user(article)
     likes = article.likes.all
     random_like = likes.offset(rand(likes.count)).first
+    other_likes_count = likes.count - 1
+
+    if other_likes_count < 100 then
+      rounded_likes_count = other_likes_count
+    elsif other_likes_count >= 100 && other_likes_count < 999 then
+      rounded_likes_count = other_likes_count.floor(-1)
+    elsif other_likes_count >= 1000 && other_likes_count < 9999 then
+      rounded_likes_count = other_likes_count.floor(-2)
+    elsif other_likes_count >= 10000 then
+      rounded_likes_count = other_likes_count.floor(-3)
+    end
+
     if likes.count == 1 then
       "#{random_like.user.account} liked this post!"
     elsif likes.count > 1 then
-      "#{random_like.user.account} and #{likes.count - 1} other liked this post!"
+      "#{random_like.user.account} and #{rounded_likes_count} other liked this post!"
     end
   end
 end
