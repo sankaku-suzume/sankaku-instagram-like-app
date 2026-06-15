bf1d7a6ff87dc869ec57c5119e1af76b0048db5f
さんかく <sankaku.suzume@gmail.com>
2026-06-16 06:00:05 +0900

    ゲストログイン機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -41,4 +41,8 @@ Rails.application.routes.draw do
       resource :like, only: [ :show, :create, :destroy ]
     end
   end
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/guest_sessions#create'
+  end
 end
